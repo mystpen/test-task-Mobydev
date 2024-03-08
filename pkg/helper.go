4a824e445dc2585580fc1118872bfba26f0bf621
dokshiye <okshieva@gmail.com>
2024-03-08 21:31:02 +0600

    feat: add helper.go

---
+++ pkg/helper.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type envelope map[string]any
+
+func writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
